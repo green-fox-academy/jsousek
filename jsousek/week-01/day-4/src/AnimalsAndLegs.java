087811393c97f6fd06e01124a66811b691cd4103
Jiri Sousek <jirisousek@outlook.com>
2018-09-20 17:30:35 +0200

    3rd commit of java3rd commit of java3rd commit of java

--- jsousek/week-01/day-4/src/AnimalsAndLegs.java
+++ jsousek/week-01/day-4/src/AnimalsAndLegs.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class AnimalsAndLegs {
     public static void main(String[] args) {
         // Write a program that asks for two integers
         // The first represents the number of chickens the farmer has
         // The second represents the number of pigs owned by the farmer
         // It should print how many legs all the animals have
+        System.out.println("Write the number of chickens");
+
+        Scanner chickenLegs = new Scanner(System.in);
+        int chicken = chickenLegs.nextInt();
+
+        System.out.println("Write the number of pigs");
+
+        Scanner pigLegs = new Scanner(System.in);
+        int pigs = pigLegs.nextInt();
+
+        System.out.println("Number of legs is: " + (pigs*4 + chicken*2));
     }
 }
