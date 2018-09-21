76ae0952be0da12cd6440f9c77d115b6aa4d643d
Jiri Sousek <jirisousek@outlook.com>
2018-09-21 11:13:25 +0200

    new clean git commit

---
+++ jsousek/week-01/day-4/src/AverageOfInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AverageOfInput {
+    public static void main(String[] args) {
+
+        Scanner firstInt = new Scanner(System.in);
+        int nr1 = firstInt.nextInt();
+
+        Scanner secondInt = new Scanner(System.in);
+        int nr2 = secondInt.nextInt();
+
+        Scanner thirdInt = new Scanner(System.in);
+        int nr3 = thirdInt.nextInt();
+
+        Scanner fourthInt = new Scanner(System.in);
+        int nr4 = fourthInt.nextInt();
+
+        Scanner fifthInt = new Scanner(System.in);
+        int nr5 = fifthInt.nextInt();
+
+
+    }
+}
--- jsousek/week-01/day-4/src/BMI.java
+++ jsousek/week-01/day-4/src/BMI.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class BMI {
     public static void main(String[] args) {
-        double massInKg = 81.2;
-        double heightInM = 1.78;
+        System.out.println("Your mass in kg");
+
+        Scanner mass = new Scanner(System.in) ;
+        double massInKg = mass.nextDouble();
+
+        System.out.println("Your height in m");
+
+        Scanner height = new Scanner(System.in);
+        double heightInM = height.nextDouble();
+
         double tempV = massInKg/heightInM;
 
-        System.out.println(tempV/heightInM);
+        System.out.println("Your BMI: "+ tempV/heightInM);
 
 
         // Print the Body mass index (BMI) based on these values
