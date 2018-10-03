e4c37869f05ba60ed48429c26866de20692abfa7
Jiri Sousek <jirisousek@outlook.com>
2018-10-03 13:24:39 +0200

    postIt

--- jsousek/week-03/Day-03/Task1/src/PostIt.java
+++ jsousek/week-03/Day-03/Task1/src/PostIt.java
@@ -1,2 +1,28 @@
+import javafx.geometry.Pos;
+
 public class PostIt {
+    String backroudcolor;
+    String text;
+    String textColor;
+
+    public PostIt(String backroudcolor, String text, String textColor){
+        this.backroudcolor = backroudcolor;
+        this.text = text;
+        this.textColor =textColor;
+
+    }
+
+    public PostIt(){
+
+    }
+
+    public static void main(String[] args) {
+        PostIt first = new PostIt("orange", "Idea1", "Blue");
+        PostIt second = new PostIt("pink", "Awesome", "black");
+        PostIt third = new PostIt("yellow", "superb", "green");
+        PostIt voidPostIt = new PostIt();
+
+    }
 }
+
+
