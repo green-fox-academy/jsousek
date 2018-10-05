1aae334be473d163280f1798517b062c71b11557
Jiri Sousek <jirisousek@outlook.com>
2018-10-05 11:29:01 +0200

    Carpet animated colors

--- jsousek/week-03/Day-05/Fractals/src/SierpinskyCarpet.java
+++ jsousek/week-03/Day-05/Fractals/src/SierpinskyCarpet.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.Random;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
@@ -9,11 +10,17 @@ public class SierpinskyCarpet {
         drawElement(0,0,600, graphics);
 
     }
+    public static int randomColor(){
+        int r =(int)(Math.random()*255);
+        return r;
+
+    }
 
     public static void drawElement (int topLx, int topLy,  int size, Graphics graphics){
         int div = size/3;
-        graphics.setColor(Color.BLACK);
+        graphics.setColor(new Color(randomColor(),randomColor(),randomColor()));
         graphics.fillRect(topLx+div, topLy+div, div, div);
+
         if (div > 4){
             drawElement(topLx, topLy, div, graphics);
             drawElement(topLx, topLy+div, div, graphics);
@@ -24,7 +31,6 @@ public class SierpinskyCarpet {
             drawElement(topLx +2*div, topLy +div, div, graphics);
             drawElement(topLx + 2*div, topLy +2*div, div, graphics);
         }
-
     }
 
     public static void mainDraw(Graphics graphics) {
@@ -52,10 +58,23 @@ public class SierpinskyCarpet {
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
         jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        ImagePanel panel = new ImagePanel();//n
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));//n
         jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         jFrame.add(new ImagePanel());
+        jFrame.add(panel);
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
+        while (true) {
+            panel.repaint();
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     static class ImagePanel extends JPanel {
