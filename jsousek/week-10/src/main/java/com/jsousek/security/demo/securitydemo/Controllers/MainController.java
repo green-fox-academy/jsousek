9cc14ad1b3ff7ebdf9138fad5ce7959014cc8202
Jiri Sousek <jirisousek@outlook.com>
2018-12-04 12:09:49 +0100

    basic http authentication

--- jsousek/week-10/src/main/java/com/jsousek/security/demo/securitydemo/Controllers/MainController.java
+++ jsousek/week-10/src/main/java/com/jsousek/security/demo/securitydemo/Controllers/MainController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "/index")
     public String homepage(){
         return "home";
+    }
+    @GetMapping(value = "/text")
+    public String textPage(){
+        return "text";
     }
 }
