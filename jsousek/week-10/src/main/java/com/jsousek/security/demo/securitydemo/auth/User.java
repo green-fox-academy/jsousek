fb0900cc299f14afb05391445ca62971b3ea68d7
Jiri Sousek <jirisousek@outlook.com>
2018-12-04 12:30:50 +0100

    user entity

---
+++ jsousek/week-10/src/main/java/com/jsousek/security/demo/securitydemo/auth/User.java
@@ -0,0 +1,20 @@
+package com.jsousek.security.demo.securitydemo.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Getter
+@Setter
+
+@Entity
+@Table(name = "USER")
+public class User {
+    @Id @GeneratedValue
+    @Column(name = "USER_ID")
+    private long id;
+    @Column(name = "USERNAME", nullable = false, unique = true)
+    private String username;
+    @Column(name = "PASSWORD")
+    private String password;
+}
